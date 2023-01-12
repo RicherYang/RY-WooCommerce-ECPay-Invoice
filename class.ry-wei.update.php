0681ba832be1a28632f5b2400288ad39acb10cf8
RicherYang <fantasyworldidvtw@gmail.com>
2023-01-12 22:14:28 +0800

    update 1.4.9
    紀錄延遲開立失敗的錯誤訊息到訂單備註。

--- class.ry-wei.update.php
+++ class.ry-wei.update.php
@@ -32,8 +32,8 @@ final class RY_WEI_update
             RY_WEI::update_option('version', '1.1.0');
         }
 
-        if (version_compare($now_version, '1.4.8.1', '<')) {
-            RY_WEI::update_option('version', '1.4.8.1');
+        if (version_compare($now_version, '1.4.9', '<')) {
+            RY_WEI::update_option('version', '1.4.9');
         }
     }
 }
--- woocommerce/ecpay-invoice-response.php
+++ woocommerce/ecpay-invoice-response.php
@@ -11,8 +11,6 @@ class RY_WEI_Invoice_Response extends RY_WEI_Invoice_Api
 
     public static function check_callback()
     {
-        RY_WEI_Invoice::log('IPN post request: ' . var_export($_POST, true));
-        RY_WEI_Invoice::log('IPN get request: ' . var_export($_GET, true));
         if (!empty($_POST)) {
             $ipn_info = wp_unslash($_POST);
             if (self::ipn_request_is_valid($ipn_info)) {
@@ -44,18 +42,35 @@ class RY_WEI_Invoice_Response extends RY_WEI_Invoice_Api
     {
         $order_id = self::get_order_id($ipn_info, RY_WEI::get_option('order_prefix'));
         if ($order = wc_get_order($order_id)) {
-            if (isset($ipn_info['invoicenumber'], $ipn_info['invoicecode'], $ipn_info['invoicedate'], $ipn_info['invoicetime'])) {
-                if (!empty($ipn_info['invoicenumber']) && $order->get_meta('_invoice_ecpay_RelateNumber') == $ipn_info['od_sob']) {
-                    $order->update_meta_data('_invoice_number', $ipn_info['invoicenumber']);
-                    $order->update_meta_data('_invoice_random_number', $ipn_info['invoicecode']);
-                    $order->update_meta_data('_invoice_date', $ipn_info['invoicedate'] . ' ' . $ipn_info['invoicetime']);
-                    $order->save_meta_data();
-                    self::die_success();
+            if (isset($ipn_info['inv_error']) && !empty($ipn_info['inv_error'])) {
+                $order->add_order_note(sprintf(
+                    /* translators: %s Error messade */
+                    __('Get invoice error: %s', 'ry-woocommerce-ecpay-invoice'),
+                    $ipn_info['inv_error']
+                ));
+            } else {
+                if (isset($ipn_info['invoicenumber'], $ipn_info['invoicecode'], $ipn_info['invoicedate'], $ipn_info['invoicetime'])) {
+                    if (!empty($ipn_info['invoicenumber']) && $order->get_meta('_invoice_ecpay_RelateNumber') == $ipn_info['od_sob']) {
+                        if (apply_filters('ry_wei_add_api_success_notice', true)) {
+                            $order->add_order_note(
+                                __('Invoice number', 'ry-woocommerce-ecpay-invoice') . ': ' . $ipn_info['invoicenumber'] . "\n"
+                                . __('Invoice random number', 'ry-woocommerce-ecpay-invoice') . ': ' . $ipn_info['invoicecode'] . "\n"
+                                . __('Invoice create time', 'ry-woocommerce-ecpay-invoice') . ': ' . $ipn_info['invoicedate'] . ' ' . $ipn_info['invoicetime'] . "\n"
+                            );
+                        }
+
+                        $order->update_meta_data('_invoice_number', $ipn_info['invoicenumber']);
+                        $order->update_meta_data('_invoice_random_number', $ipn_info['invoicecode']);
+                        $order->update_meta_data('_invoice_date', $ipn_info['invoicedate'] . ' ' . $ipn_info['invoicetime']);
+                        $order->save_meta_data();
+                        self::die_success();
+                    } else {
+                        RY_WEI_Invoice::log('Error invoice info', 'warning');
+                    }
                 } else {
-                    RY_WEI_Invoice::log('Error invoice info', 'warning');
+                    RY_WEI_Invoice::log('Lost invoice info', 'warning');
                 }
             }
-            RY_WEI_Invoice::log('Lost invoice info', 'warning');
         } else {
             RY_WEI_Invoice::log('Order not found', 'warning');
         }
