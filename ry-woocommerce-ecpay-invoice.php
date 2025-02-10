31187b82bd6b8c95f0be537b82a856086e4c1b0c
Richer Yang <fantasyworldidvtw@gmail.com>
2025-02-10 21:39:25 +0800

    修正可能的函式回傳值型態錯誤

--- ry-woocommerce-ecpay-invoice.php
+++ ry-woocommerce-ecpay-invoice.php
@@ -4,7 +4,7 @@
  * Plugin Name: RY ECPay Invoice for WooCommerce
  * Plugin URI: https://ry-plugin.com/ry-woocommerce-ecpay-invoice
  * Description: WooCommerce order invoice for ecpay
- * Version: 2.0.4
+ * Version: 2.0.5
  * Requires at least: 6.6
  * Requires PHP: 8.0
  * Requires Plugins: woocommerce
@@ -22,7 +22,7 @@
 
 function_exists('plugin_dir_url') or exit('No direct script access allowed');
 
-define('RY_WEI_VERSION', '2.0.4');
+define('RY_WEI_VERSION', '2.0.5');
 define('RY_WEI_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('RY_WEI_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('RY_WEI_PLUGIN_BASENAME', plugin_basename(__FILE__));
