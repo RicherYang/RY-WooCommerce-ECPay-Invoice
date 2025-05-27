b250fe5d1946c9ffad7f53e7f81f1945046e0903
Richer Yang <fantasyworldidvtw@gmail.com>
2025-05-27 21:09:17 +0800

    修正更新頁面與其他外掛可能的不相容問題

--- woocommerce/admin/view/html-version-info.php
+++ woocommerce/admin/view/html-version-info.php
@@ -5,7 +5,6 @@
     <td class="forminp">
         <?php esc_html_e('Now Version:', 'ry-woocommerce-ecpay-invoice'); ?> <?php echo esc_html($version); ?>
         <?php if ($version_info && version_compare($version, $version_info['version'], '<')) { ?>
-        <?php set_site_transient('update_plugins', []); ?>
         <br>
         <span style="color:blue"><?php esc_html_e('New Version:', 'ry-woocommerce-ecpay-invoice'); ?></span> <?php echo esc_html($version_info['version']); ?>
         <a href="<?php echo esc_url(wp_nonce_url(self_admin_url('update.php?action=upgrade-plugin&plugin=' . RY_WEI_PLUGIN_BASENAME), 'upgrade-plugin_' . RY_WEI_PLUGIN_BASENAME)); ?>">
